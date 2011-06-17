5927c7cbb2ec85570f4fadc6c754c6a5fc583956
Jed Davis <jld@panix.com>
2011-06-17 12:46:58 -0700

    Speak, but also listen.

--- speech.ml
+++ speech.ml
@@ -33,4 +33,30 @@ let card_of_string = function
   | "copy" -> Copy
   | "revive" -> Revive
   | "zombie" -> Zombie
+  | s -> failwith ("Bad card name "^s)
 
+let utter fp i st =
+  match st with
+    Left c -> Printf.fprintf fp "1\n%s\n%d\n%!" (string_of_card c) i
+  | Right c -> Printf.fprintf fp "2\n%d\n%s\n%!" i (string_of_card c)
+
+let speak fp i =
+  List.iter (utter fp i)
+
+let record s i ri =
+  let fp = open_out s in
+  speak fp i ri;
+  close_out fp
+
+let hear fp =
+  match input_line fp with
+    "1" -> 
+      let card = card_of_string (input_line fp) in
+      let slot = int_of_string (input_line fp) in
+      (slot, Left card)
+  | "2" ->
+      let slot = int_of_string (input_line fp) in
+      let card = card_of_string (input_line fp) in
+      (slot, Right card)
+  | l ->
+      failwith ("Bad side "^l)
